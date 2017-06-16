c907de912778d3171d8913ad81e651b5ab191b20
toelapiut <toelapiut7@gmail.com>
2017-06-16 16:46:12 +0300

    adding fonts and modification

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,13 @@ Referee.prototype.switchPlayer =  function (){
     this.players[1].turn = 0;
   }
 
-};
+}
 
 Referee.prototype.runningTotal = function (player) {
   player.score += player.runningTotal;
   player.runningTotal = 0;
-};
+}
+
 Referee.prototype.throw = function (){
   var result = Math.floor((Math.random() * 6) + 1);
   if(this.gameover === 0){
@@ -48,7 +49,8 @@ Referee.prototype.throw = function (){
       this.switchPlayer();
     }
   }
-};
+}
+
 Referee.prototype.hold = function (){
   if (this.players[0].turn === 1){
     this.runningTotal(this.players[0]);
@@ -59,16 +61,16 @@ Referee.prototype.hold = function (){
     this.switchPlayer();
 
   }
-};
+}
 
 Referee.prototype.pickPlayer = function (){
   this.players[0].turn = 1;
-};
+}
 Referee.prototype.checkGame = function (){
   if (this.players[0].score >= 100){
     this.players[1].turn = 0;
     this.players[0].turn = 0;
-    this.winner = "Player 1 ";
+    this.winner = "Player 1 ";;
     this.gameover = 1;
   }else if (this.players[1].score >= 100){
     this.players[1].turn = 0;
@@ -76,7 +78,8 @@ Referee.prototype.checkGame = function (){
     this.winner = "Player 2 ";
     this.gameover = 1;
   }
-};
+}
+
 
 //interface functions
 function switchClass(player1, player2){
@@ -104,3 +107,65 @@ function showDice (dice){
   }
 
 }
+
+
+
+
+
+
+
+$(document).ready(function (){
+  var gameChoice = $('input:radio[name=gameChoice]:checked').val();
+  var player1 = new Player ("mister");
+  var player2 = new Player (gameChoice);
+  var toelTheReferee = new Referee ();
+
+
+  toelTheReferee.players.push(player1, player2);
+  toelTheReferee.pickPlayer();
+
+
+
+  $("#gameChoice").click(function (){
+    gameChoice = $('input:radio[name=gameChoice]:checked').val();
+    if (gameChoice === "ai"){
+      // console.log("ai");
+      toelTheReferee.players[1].playerID = "ai";
+
+      //
+
+    }
+    $(this).parent().parent().fadeOut(700, function (){
+      $("#twoPlayer").fadeIn();
+    });
+  });
+
+  setInterval(function (){
+    if (player2.playerID === "ai" && player2.turn ===1 ){
+      if (player2.runningTotal <= 15){
+          toelTheReferee.throw();
+          $("#hold").hide();
+          $("#roll").hide();
+      }else {
+        toelTheReferee.hold();
+        $("#hold").fadeIn();
+        $("#roll").fadeIn();
+      }
+      if(player2.turn ===0){
+        $("#hold").fadeIn();
+        $("#roll").fadeIn();
+      }
+
+
+
+      toelTheReferee.checkGame();
+      if (toelTheReferee.gameover === 1){
+        $("#winner").show().text(toelTheReferee.winner + "wins!!!");
+      }
+      showDice(toelTheReferee.dice);
+      showScore(player1, player2);
+      switchClass(player1, player2);
+
+    }
+
+  }, 1000);
